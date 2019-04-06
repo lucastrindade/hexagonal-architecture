3b5c2eee6005b6709a0e9c79c26b13d9cc97f7df
Lucas Trindade <lucas.silva@autoavaliar.com.br>
2019-04-06 17:35:36 -0300

    adiciona getters e setters para Order

--- src/Order/Domain/Entity/Order.php
+++ src/Order/Domain/Entity/Order.php
@@ -11,12 +11,52 @@ class Order
     protected $id;
 
     /**
+     * @var float $value
+     */
+    protected $value;
+
+    /**
      * Order constructor
      *
      * @param integer $id
+     * @param float $value
      */
-    public function __construct(int $id)
+    public function __construct(int $id, float $value)
     {
         $this->id = $id;
+        $this->value = $value;
     }
+
+    /**
+     * Getter for ID
+     *
+     * @return integer
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter for value
+     *
+     * @return float
+     */
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+
+    /**
+     * Setter for value
+     *
+     * @param float $value
+     * @return void
+     */
+    public function setValue(float $value)
+    {
+        $this->value = $value;
+    }
+
+    // some actions
 }
